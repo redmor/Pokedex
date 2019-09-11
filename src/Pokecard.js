f15e25cb546d85c064e712e5ce4f023f75fa347c
Oussama Er rabili <errabili.oussama@gmail.com>
2019-09-10 22:00:17 -0400

    Fancier images & Id fix

--- src/Pokecard.js
+++ src/Pokecard.js
@@ -1,30 +1,29 @@
-import React, {
-    Component
-} from 'react';
+import React, {Component} from 'react';
 import './Pokecard.css';
 
-const POKE_API = 'https://raw.githubusercontent.com/pokeapi/sprites/master/sprites/pokemon/';
+//const POKE_API = 'https://raw.githubusercontent.com/pokeapi/sprites/master/sprites/pokemon/';
+const POKE_API = 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/';
 
 class Pokecard extends Component {
-    render() {
-        const imgSrc = `${POKE_API}${this.props.id}.png`;
-        return ( <
-            div className = 'Pokecard' >
-            <
-            h1 > {
-                this.props.name
-            } < /h1> <
-            img src = {
-                imgSrc
+    render(){
+        const idFix = (id) => {
+            if(id <= 9){
+                id = `00${id}`;
+            }else if(id <= 99){
+                id = `0${id}`;
+            }else{
+                id = id;
             }
-            /> <
-            div > {
-                this.props.type
-            } < /div> <
-            div > {
-                this.props.exp
-            } < /div> <
-            /div>
+            return id;
+        }
+        const imgSrc = `${POKE_API}${idFix(this.props.id)}.png`;
+        return(
+            <div className='Pokecard'>
+                <h1>{this.props.name}</h1>
+                <img src={imgSrc} />
+                <div>{this.props.type}</div>
+                <div>{this.props.exp}</div>
+            </div>
         );
     }
 }
